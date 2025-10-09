8054605376d5203b9aae1ec27cad2aaf276c515a
giadh-ai <giadh@binhduong-aptech.edu.vn>
2025-10-09 07:47:19 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./styles.css";
+// @ts-ignore
+import Home from "./Home";
+// @ts-ignore
+import Layout from "./Layout";
+// @ts-ignore
+import Trang1 from "./Trang1";
+// @ts-ignore
+import Chitietsanpham from "./Chitietsanpham";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  //return <Layout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="trang1" element={<Trang1 />} />
+          <Route path="sanpham/:id" element={<Chitietsanpham />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,51 @@
+import "./css/main.css";
+
+const Home = () => {
+  var tenlop = "K19 Cao đẳng Kinh tế TP.HCM";
+  var ngay = 2;
+  var thang = 10;
+  var dssv = ["Nguyễn Văn An", "Trần Văn Bình", "Hà Văn Cường"];
+  var ds2 = [
+    { id: 1, hoten: "Nguyễn Văn An", lop: "K19", email: "abc@1234.edu.vn" },
+    { id: 2, hoten: "Trần Văn Bình", lop: "K19", email: "abc@1234.edu.vn" },
+    { id: 3, hoten: "Hà Văn Cường", lop: "K19", email: "abc@cuong.edu.vn" },
+    { id: 4, hoten: "Nguyễn Văn Dũng", lop: "K19", email: "abc@cuong.edu.vn" },
+  ];
+
+  return (
+    <div className="App">
+      <h1>Xin chào {tenlop}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <p>Danh sách sinh viên </p>
+        {dssv.map((sv) => {
+          return <p>{sv}</p>;
+        })}
+      </div>
+      <div>
+        <p>DANH SÁCH SINH VIÊN 2</p>
+        <div>
+          <table className="my-table">
+            <tr>
+              <td>Mã SV</td>
+              <td>Họ tên</td>
+              <td>Lớp</td>
+              <td>Email</td>
+            </tr>
+            {ds2.map((sv) => {
+              return (
+                <tr>
+                  <td>{sv.id}</td>
+                  <td>{sv.hoten}</td>
+                  <td>{sv.lop}</td>
+                  <td>{sv.email}</td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
