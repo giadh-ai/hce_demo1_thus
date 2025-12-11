2a2e55082b6421fb0cac6045a968ab80fb7acaff
Hoàng Gia Edu <giadh@binhduong-aptech.edu.vn>
2025-12-11 12:57:42 +0000

    Tao đồng bộ nhé. Chúng mày update

--- src/ListProducts_SP_Admin.jsx
+++ src/ListProducts_SP_Admin.jsx
@@ -21,7 +21,7 @@ const ListProducts_SP_Admin = () => {
   }, []);
 
   const handleDelete = async (id) => {
-    if (window.confirm("Bạn có chắc muốn xóa sản phẩm này không?")) {
+    if (window.confirm("Bạn có chắc muốn xóa sản phẩm này không11111?")) {
       const { error } = await supabase.from("product1").delete().eq("id", id);
       if (error) alert("Lỗi khi xóa: " + error.message);
       else fetchProducts();
